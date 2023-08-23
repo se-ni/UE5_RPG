4f6e590f15dcdfdf7a865cd392cc49dd2e446a1f
se-ni <20000109s@gmail.com>
2023-08-23 12:49:39 +0900

    MainPlayerCharacter projectile effect init commit

---
+++ RPG_Project/Source/RPG_Project/Global/SubClassData.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "UObject/NoExportTypes.h"
+#include "SubClassData.generated.h"
+
+/**
+ * 
+ */
+USTRUCT(BlueprintType)
+struct RPG_PROJECT_API FSubClassData : public FTableRowBase
+{
+	GENERATED_BODY()
+	
+	FSubClassData() {}
+	~FSubClassData() {}
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+		TSubclassOf<UObject> Object;
+};
