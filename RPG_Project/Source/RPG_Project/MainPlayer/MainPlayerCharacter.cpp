36e890a8c0482f79eed5315023e4b23267a70448
se-ni <20000109s@gmail.com>
2023-08-24 18:15:20 +0900

    MainPlayer remake commit

--- RPG_Project/Source/RPG_Project/MainPlayer/MainPlayerCharacter.cpp
+++ RPG_Project/Source/RPG_Project/MainPlayer/MainPlayerCharacter.cpp
@@ -52,26 +52,14 @@ void AMainPlayerCharacter::AnimNotifyBegin(FName NotifyName, const FBranchingPoi
 		UGlobalGameInstance* Inst = GetWorld()->GetGameInstance<UGlobalGameInstance>();
 
 		TSubclassOf<UObject> Effect = Inst->GetSubClass(TEXT("Effect"));
-
 		if (nullptr != Effect)
 		{
-			FTransform Trans;
-			FVector Pos;
-			//TArray<UActorComponent*> MeshEffects = GetComponentsByTag(USceneComponent::StaticClass(), TEXT("WeaponEffect"));
-			//TArray<UActorComponent*> StaticMeshs = GetComponentsByTag(USceneComponent::StaticClass(), TEXT("WeaponMesh"));
+			AActor* Actor = GetWorld()->SpawnActor<AActor>(Effect);
+			FVector effectloc = FVector(200.f, 0.f, 0.f) + GetActorLocation();
+			Actor->SetActorLocation(effectloc);
 
-			//USceneComponent* EffectCom = Cast<USceneComponent>(MeshEffects[0]);
-			//Pos = EffectCom->GetComponentToWorld().GetLocation(); // BP_MainPlayerCharacter의 Weapon Effect의 position Get 
-
-			{
-				// 효과 만들기
-				AActor* Actor = GetWorld()->SpawnActor<AActor>(Effect);
-
-				Actor->SetActorLocation(Pos);
-				Actor->SetActorRotation(GetActorRotation());
-				Actor->Destroy();
-			}
 		}
+
 }
 
 // Called every frame
